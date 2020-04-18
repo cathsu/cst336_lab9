964ee0cd11d87d5f47e3cb17f88a2e0eb012b0fb
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-04-18 04:04:14 +0000

    Reconfigure database

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.set('view engine', 'ejs');
 
 /* Configure MySQL DBMS */
 const connection = mysql.createConnection({
-    host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'o81c78k2qfme9lwj',
-    password: 'ac13p04qck3oipad',
-    database: 'pu3efiquen3026ei', 
+    host: 'localhost',
+    user: 'cathsu',
+    password: 'cathsu',
+    database: 'quotes_db', 
     multipleStatements: true
 });
 
